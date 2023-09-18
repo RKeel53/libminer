f5b9c6fcde0f75a86d35770f3f8f76d20999b4aa
Robin Keeley <robin.j.keeley@gmail.com>
2023-09-18 12:21:05 -0500

    Create documentation for lib_summary()

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,3 +1,12 @@
+#' R Library Summary
+#'
+#' Provide a brief summary of the package libraries on your machine.
+#'
+#' @return A `data.frame` containing the count of packages in each of the user's libraries
+#' @export
+#'
+#' @examples
+#' lib_summary()
 lib_summary <- function() {
   pkgs <- utils::installed.packages() # returns a character matrix of the installed packages and some information and where it is stored
   pkg_tbl <- table(pkgs[, "LibPath"]) # Making a table and extracting the LibPath
