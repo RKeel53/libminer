baefc806e56bfc80c793d7e82b50d8606e9a816b
Robin Keeley <robin.j.keeley@gmail.com>
2023-09-18 16:40:25 -0500

    Factor out calculate_sizes and use map_dbl

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -14,18 +14,21 @@ lib_summary <- function(sizes = FALSE) {
   if (!is.logical(sizes)){
     stop("sizes must be a logical value (TRUE/FALSE)")
   }
+
   pkgs <- utils::installed.packages() # returns a character matrix of the installed packages and some information and where it is stored
   pkg_tbl <- table(pkgs[, "LibPath"]) # Making a table and extracting the LibPath
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE) # turn it into a df
   names(pkg_df) <- c("Library", "n_packages") # change the names
   if (sizes) {
-    pkg_df$lib_size <- vapply(
-      pkg_df$Library,
-      function(x) {
-        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
-      },
-      FUN.VALUE = numeric(1)
-    )
+    pkg_df <- calculate_sizes(pkg_df)
   }
   pkg_df # return the df
+}
+
+calculate_sizes <- function(df){
+  df$lib_size <- map_dbl(
+    df$Library,
+    ~ sum(fs::file_size(fs::dir_ls(.x, recurse = TRUE)))
+  )
+  df
 }
