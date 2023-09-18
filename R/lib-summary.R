ea81247b8f5ff83b97b0d74fdf930aafdefbeffa
Robin Keeley <robin.j.keeley@gmail.com>
2023-09-18 15:43:24 -0500

    Add sizes argument to lib_summary()

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -2,15 +2,30 @@
 #'
 #' Provide a brief summary of the package libraries on your machine.
 #'
-#' @return A `data.frame` containing the count of packages in each of the user's libraries
+#' @param sizes A logical value indicating whether to calculate sizes. Default `FALSE`
+#'
+#' @return A `data.frame` containing the count of packages in each of the user's libraries. If `sizes == TRUE`, a column of library sizes will be added
 #' @export
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+#' lib_summary(sizes = TRUE)
+lib_summary <- function(sizes = FALSE) {
+  if (!is.logical(sizes)){
+    stop("sizes must be a logical value (TRUE/FALSE)")
+  }
   pkgs <- utils::installed.packages() # returns a character matrix of the installed packages and some information and where it is stored
   pkg_tbl <- table(pkgs[, "LibPath"]) # Making a table and extracting the LibPath
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE) # turn it into a df
   names(pkg_df) <- c("Library", "n_packages") # change the names
+  if (sizes) {
+    pkg_df$lib_size <- vapply(
+      pkg_df$Library,
+      function(x) {
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      },
+      FUN.VALUE = numeric(1)
+    )
+  }
   pkg_df # return the df
 }
--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -8,5 +8,11 @@ test_that("lib_summary returns expected results", {
 })
 
 test_that("lib_summary fails appropriately", {
-  expect_error(lib_summary("foo"), "unused argument") #uses regex to match the error message (don't have to be exact)
+  expect_error(lib_summary("foo"), "sizes must be a logical value") #uses regex to match the error message (don't have to be exact)
+})
+
+test_that("sizes argument works", {
+  res <- lib_summary(sizes = TRUE)
+  expect_equal(names(res), c("Library", "n_packages", "lib_size"))
+  expect_type(res$lib_size, "double")
 })
